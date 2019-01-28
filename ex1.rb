49f79fd61d4ec59b01801fe303a0af163f8ab209
Charlie Egan <c.egan9292@gmail.com>
2019-01-28 17:57:01 +0000

    Add files via upload

---
+++ ex1.rb
@@ -0,0 +1,10 @@
+# prints sentences (strings)
+
+puts "Hello world!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "line 8"
---
+++ ex3.rb
@@ -0,0 +1,25 @@
+# puts sentence (string)
+puts "I will now count my chickens:"
+
+# puts a string and a equation using string interpolation
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+# puts the result of a calculation
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+# compares the sums - puts a boolean value
+puts 3 + 2 < 5 - 7
+# uses string interpolation
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why its false."
+
+puts "How about some more."
+# uses string interpolation and uses comparison operators
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
